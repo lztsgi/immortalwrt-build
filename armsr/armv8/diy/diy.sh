6a2690f8100b9ea64c180bf556273654f1b38363
lztsgi <160443986+lztsgi@users.noreply.github.com>
2026-05-31 14:27:52 +0800

    Update diy.sh

--- armsr/armv8/diy/diy.sh
+++ armsr/armv8/diy/diy.sh
@@ -9,6 +9,9 @@ git clone https://github.com/ophub/luci-app-amlogic --depth=1 clone/amlogic
 # Adjust packages
 rm -rf feeds/luci/applications/luci-app-passwall
 cp -rf clone/amlogic/luci-app-amlogic feeds/luci/applications/
+# N1 uses openwrt_packit/flippy Amlogic images; prefer luci-app-amlogic for upgrades.
+# Disable generic LuCI attended sysupgrade to avoid producing incompatible armsr images.
+sed -i '/luci-app-attendedsysupgrade/d' feeds/luci/collections/luci/Makefile
 
 # Clean packages
 rm -rf clone
