cb8620c685cb5b3a8a2f6a1d1b7d27d88ad2f13d
lztsgi <160443986+lztsgi@users.noreply.github.com>
2026-05-31 13:11:21 +0800

    Update diy.sh

--- armsr/armv8/diy/diy.sh
+++ armsr/armv8/diy/diy.sh
@@ -9,7 +9,6 @@ git clone https://github.com/ophub/luci-app-amlogic --depth=1 clone/amlogic
 # Adjust packages
 rm -rf feeds/luci/applications/luci-app-passwall
 cp -rf clone/amlogic/luci-app-amlogic feeds/luci/applications/
-sed -i '/luci-app-attendedsysupgrade/d' feeds/luci/collections/luci/Makefile
 
 # Clean packages
 rm -rf clone
